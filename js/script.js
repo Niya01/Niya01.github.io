ce817c5b49591e4e37d0c04bcd91e1491d8e9d58
shaniya <shaniyaw619@gmail.com>
2015-07-30 13:58:05 -0500

    Edit html and css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 function submitInfo() {
 	var name = $("#nameinput").val();
-	var name = $("emailinput").val();
-	alert(name);
-}
+	var email = $("#emailinput").val();
 
-alert("hello?");
+	if (name && email){
+		// this is where you make ajax call
+		alert("Is your name " + name + "? Is your email " + email + "?" );
+	} else {
+		// tell the user they didn't give the right info
+		if (name){
+			alert("I didnt recieve you email address.");
+		} else if (email) {
+			alert("I didnt get your name.");
+		} else{
+			alert("I didnt get your name or email.")
+		}
+	}
+}
