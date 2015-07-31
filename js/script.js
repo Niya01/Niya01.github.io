4df7f9940e2291d5f9fbc893fcdda0ee4be048a9
shaniya <shaniyaw619@gmail.com>
2015-07-31 11:10:52 -0500

    javascript

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-function submitInfo() {
-	var name = $("#nameinput").val();
-	var email = $("#emailinput").val();
+// function submitInfo() {
+// 	var name = $("#nameinput").val();
+// 	var email = $("#emailinput").val();
 
-	if (name && email){
-		// this is where you make ajax call
-		alert("Is your name " + name + "? Is your email " + email + "?" );
-	} else {
-		// tell the user they didn't give the right info
-		if (name){
-			alert("I didnt recieve you email address.");
-		} else if (email) {
-			alert("I didnt get your name.");
-		} else{
-			alert("I didnt get your name or email.")
-		}
-	}
+// 	if (name && email){
+// 		// this is where you make ajax call
+// 		alert("Is your name " + name + "? Is your email " + email + "?" );
+// 	} else {
+// 		// tell the user they didn't give the right info
+// 		if (name){
+// 			alert("I didnt recieve you email address.");
+// 		} else if (email) {
+// 			alert("I didnt get your name.");
+// 		} else{
+// 			alert("I didnt get your name or email.")
+// 		}
+// 	}
+// }
+
+// function submitInfo(){
+// 	$.ajax ({
+// 		url:"http://347e6913.ngrok.com/test"
+// 	})
+// 	.done(function(data){
+// 		alert(data.message);
+// 	})
+// 	.error(function (error){
+// 		alert(error.message);
+// 	});
+// }
+
+function submitInfo(){
+	var data= $("#nameinput").val();
+	$. ajax({
+		url:"http://347e6913.ngrok.com/test",
+		method: "POST",
+		dataType: "JSON",
+		data: { message: data }
+	})
+	.done (function (data) {
+		alert(data.message);
+	})
+	.error (function (error){
+		alert(data.message);
+	});
 }
