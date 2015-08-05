e5fb6af9c00910e7a93bafca3c9d14ff60607c24
shaniya <shaniyaw619@gmail.com>
2015-08-05 14:17:15 -0500

    Connected backend

--- js/script.js
+++ js/script.js
@@ -1,62 +1,50 @@
-// function submitInfo() {
-// 	var name = $("#nameinput").val();
-// 	var email = $("#emailinput").val();
-
-// 	if (name && email){
-// 		// this is where you make ajax call
-// 		alert("Is your name " + name + "? Is your email " + email + "?" );
-// 	} else {
-// 		// tell the user they didn't give the right info
-// 		if (name){
-// 			alert("I didnt recieve you email address.");
-// 		} else if (email) {
-// 			alert("I didnt get your name.");
-// 		} else{
-// 			alert("I didnt get your name or email.")
-// 		}
-// 	}
-// }
-
-// function submitInfo(){
-// 	$.ajax ({
-// 		url:"http://347e6913.ngrok.com/test"
-// 	})
-// 	.done(function(data){
-// 		alert(data.message);
-// 	})
-// 	.error(function (error){
-// 		alert(error.message);
-// 	});
-// }
-
-// function submitInfo(){
-// 	var data= $("#nameinput").val();
-// 	$. ajax({
-// 		url:"http://347e6913.ngrok.com/test",
-// 		method: "POST",
-// 		dataType: "JSON",
-// 		data: { message: data }
-// 	})
-// 	.done (function (data) {
-// 		alert(data.message);
-// 	})
-// 	.error (function (error){
-// 		alert(data.message);
-// 	});
-// }
+var url = "http://hbcuchicagoconnection.azurewebsites.net/";
 
 function submitInfo () {
-	var email = $("#emailinput").val();
-	$.ajax({
-		type: "POST",
-		url: "http://localhost:3000/join",
-		data:{ email: email },
-		success: function (data) { console.log(data), alert("Success!"); }
-	});
+	var email = $("#emailInput").val();
+	var name = $("#nameInput").val();
+	if(name && isValidEmailAddress(email)) {
+		$.ajax({
+			type: "POST",
+			url: url + "join",
+			data: { 
+				email: email,
+				name: name 
+			},
+			success: function (data) {
+				alert("Success!")
+			},
+			error: function (error) {
+				var code = error.status;
+				switch (code) {
+					case 500:
+						alert("Some went wrong!");
+						break;
+					case 400:
+						alert("Bad input!");
+						break;
+					case 409:
+						alert("Email already on the list!");
+						break;
+					default:
+						break;
+				}
+			}
+		});
+		$("#emailInput").val("");
+		$("#nameInput").val("");
+	} else {
+		alert("Please provide a name and a vaild email.")
+	}	
 }
 
-$(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
-});
+function isValidEmailAddress(email) {
+	var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	return pattern.test(email);
+}
+
+// $(document).ready(function(){
+//   $('.your-class').slick({
+//     setting-name: setting-value
+//   });
+// });
